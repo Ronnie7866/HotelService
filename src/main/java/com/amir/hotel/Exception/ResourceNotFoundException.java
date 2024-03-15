7a4391ff7f58188674bdc7b19db12dc75bc0e504
Mohammad Amir <mohdamir7505@gmail.com>
2024-03-15 13:03:38 -0700

    commit

--- src/main/java/com/amir/hotel/Exception/ResourceNotFoundException.java
+++ src/main/java/com/amir/hotel/Exception/ResourceNotFoundException.java
@@ -1,2 +1,11 @@
-package com.amir.hotel.Exception;public class ResourceNotFoundException {
+package com.amir.hotel.Exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String s) {
+        super(s);
+    }
+
+    public ResourceNotFoundException() {
+        super("Resource not found ");
+    }
 }
--- src/main/java/com/amir/hotel/Service/HotelService.java
+++ src/main/java/com/amir/hotel/Service/HotelService.java
@@ -1,2 +1,19 @@
-package com.amir.hotel.Service;public interface HotelService {
+// HotelService.java
+package com.amir.hotel.Service;
+
+import com.amir.hotel.Entities.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+
+    Hotel createHotel(Hotel hotel);
+
+    List<Hotel> getAllHotel();
+
+    Hotel get(Integer id);
+
+    Hotel updateHotel(Hotel hotel); // Add an update method
+
+    Hotel deleteHotel(Integer id); // It's better to return void for delete operations
 }
